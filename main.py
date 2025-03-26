6c1b29eaa097ebf8c2435d4ba253ac64a3fac968
VivekPatil-2006 <vivek.j.patil2006@gmail.com>
2025-03-26 20:21:52 +0530

    Simplified API to return Hello message

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 # Initialize FastAPI app
 app = FastAPI()
 
-@app.get("/")
+@app.api_route("/", methods=["GET", "HEAD"])
 def read_root():
     """Return a simple Hello message"""
     return {"message": "Hello"}
